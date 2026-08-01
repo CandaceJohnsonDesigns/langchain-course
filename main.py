bbbad40ceac2fc3609f3204ccc250aaf97518be0
Candace Johnson <candace@candacejohnsondesigns.com>
2026-07-31 18:57:31 -0700

    feat: Add schemas for agent response and source in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import langchain
+from typing import List
 
+from pydantic import BaseModel, Field  
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,10 +10,27 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """
+    Schema for a source used by the agent
+    """
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """
+    Schema for the agent's response with answer and sources
+    """
+
+    answer: str = Field(description="The content of the response")
+    sources: List[Source] = Field(
+        default_factory=list, 
+        description="The sources used to generate the response"
+        )
 
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
